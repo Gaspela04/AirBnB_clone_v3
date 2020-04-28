7699fb3afe362a3b98999707bc49d23385065963
Samir millan orozco <samirmillanorozco@hotmail.com>
2020-04-28 15:18:18 +0000

    error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 """Api hbnb"""
+from api.v1.views import app_views
 from models import storage
 from flask import Flask
+from flask import Flask, Blueprint, jsonify
 
 app = Flask(__name__)
 
-from api.v1.views import app_views
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def strg_close(x):
     """Close session storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(self):
+    """404 error"""
+    return jsonify({"error": "Not found"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 import json
+from models import storage
+
 
 @app_views.route('/status')
 def status():
     return {
         "status": "OK"
     }
+
+@app_views.route('/api/v1/stats', strict_slashes=False)
+def stats():
+    """ retrieve the number of each object by type """
+    return {"amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User')}
