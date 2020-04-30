2841acaae58ae310b2afdf5728d8023dc073a8af
Samir millan orozco <samirmillanorozco@hotmail.com>
2020-04-30 04:54:24 +0000

    Fix import blue

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Api hbnb"""
 from models import storage
-from flask import Flask, jsonify
+from flask import Flask, Blueprint, jsonify
 from flask_cors import CORS
 from api.v1.views import app_views
 from os import getenv
