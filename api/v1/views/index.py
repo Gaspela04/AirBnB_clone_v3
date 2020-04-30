426c7465b7655966b167ea1f2bd35f7d68d44fe4
Samir millan orozco <samirmillanorozco@hotmail.com>
2020-04-30 04:49:20 +0000

    Status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,14 +2,13 @@
 """index file"""
 from flask import Flask
 from api.v1.views import app_views
-import json
 from models import storage
 
 
-@app_views.route('/status')
+@app_views.route('/api/v1/status', strict_slashes=False)
 def status():
-    """ Return status OK """
-    return jsonify({"status": "OK"})
+    """ return a JSON """
+    return {"status": "OK"}
 
 
 @app_views.route('/stats', methods=['GET'])
