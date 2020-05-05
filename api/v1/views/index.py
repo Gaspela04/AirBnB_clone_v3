da3dcc976cc36b485596224665acffae6792575e
Mauricio-KND <1299@holbertonschool.com>
2020-05-05 00:50:01 +0000

    Fixing API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/api/v1/status', strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status():
     """ return a JSON """
     return {"status": "OK"}
